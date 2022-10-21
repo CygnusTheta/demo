cf8fcd73badc2d5979b47c180539183029751330
cygnustheta <crypto@cygnustheta.com>
2022-10-21 15:07:03 -0400

    Initial Commit

---
+++ src/main/java/com/maventest/test/App.java
@@ -0,0 +1,20 @@
+package com.maventest.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(add(1,2));
+    }
+
+
+    public static int add(int a,int b) {
+       return a+b; 
+    }
+    
+}
---
+++ src/test/java/com/maventest/test/AppTest.java
@@ -0,0 +1,25 @@
+package com.maventest.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertEquals(3,App.add(1,2));
+    }
+
+    @Test
+    public void testAdd() {
+
+        assertTrue(true);
+    }
+}
